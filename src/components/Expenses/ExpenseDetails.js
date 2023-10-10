0282466a720f9fd4e609ddce65c1b33e66368699
AVINASH KUMAR <Aviproo>
2023-10-10 15:29:59 +0530

    Add butto increase expense, title by useState hook

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 const Expensedetails = (props) => {
-  const deleteExpense = () => {
-    console.log("clicked");
+  let [amount, setamount] = useState(props.amount);
+  let [title, setTitle] = useState(props.title);
+  let increseExpense = () => {
+    setamount(amount + 100);
+  };
+  let changeTitle = () => {
+    setTitle("updated title");
   };
   return (
     <div className="expense-item__description">
-      <h2>{props.title}</h2>
-      <div className="expense-item__price">${props.amount}</div>
+      <h2>{title}</h2>
+      <button onClick={changeTitle}>Change Title</button>
+      <div className="expense-item__price">${amount}</div>
       <div className="expense-item__price">{props.location}</div>
-      <button onClick={deleteExpense}>Delete Expense</button>
+      <button onClick={increseExpense}>Increse Expense</button>
     </div>
   );
 };
