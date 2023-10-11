77330f9a04f24be7325c3d43aab6cbc99ef34d70
AVINASH KUMAR <Aviproo>
2023-10-11 17:04:54 +0530

    Conditional Rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -13,22 +14,14 @@ const Expenses = (props) => {
   const filterExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() == filteredYear;
   });
+
   return (
     <div>
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filterExpenses.map((item) => (
-        <Card key={item.id} classname="expenses">
-          <ExpenseItem
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-            location={item.location}
-          />
-        </Card>
-      ))}
+      <ExpensesList items={filterExpenses} />
     </div>
   );
 };
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,26 @@
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+const ExpensesList = (props) => {
+  let expenseContent;
+  if (props.items.length == 1) {
+    expenseContent = <p>"Only single Expense here. Please add more..."</p>;
+  }
+  if (props.items.length === 0) {
+    return <h4 className="expenses-List__fallback">No items present</h4>;
+  }
+  return (
+    <ul className="expenses-list">
+      {props.items.map((item) => (
+        <div key={item.id} classname="expenses">
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+            location={item.location}
+          />
+        </div>
+      ))}
+    </ul>
+  );
+};
+export default ExpensesList;
