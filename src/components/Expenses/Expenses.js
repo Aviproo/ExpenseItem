ec649d2354b31690850833f0074c920fe6b7201d
AVINASH KUMAR <Aviproo>
2023-10-11 16:21:11 +0530

    Implementing the filter feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
 const Expenses = (props) => {
-  return props.items.map((item) => (
-    <Card key={item.id} classname="expenses">
-      <ExpenseItem
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-        location={item.location}
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() == filteredYear;
+  });
+  return (
+    <div>
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-    </Card>
-  ));
+      {filterExpenses.map((item) => (
+        <Card key={item.id} classname="expenses">
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+            location={item.location}
+          />
+        </Card>
+      ))}
+    </div>
+  );
 };
 export default Expenses;
